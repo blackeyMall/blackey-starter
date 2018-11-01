10aeef762c5ee5dc102a40f4eeb44f197e2d892b
befairytale@163.com <befairytale@163.com>
2018-11-01 10:28:18 +0800

    修改bo

--- spring-boot-starter-blackey-jpa/src/main/java/com/blackey/jpa/common/BaseListOutput.java
+++ spring-boot-starter-blackey-jpa/src/main/java/com/blackey/jpa/common/BaseListOutput.java
@@ -16,54 +16,54 @@ import java.util.List;
  * @author blackey
  * @date 2018/11/1
  */
-public abstract class BaseListOutput<From, To extends BaseBo<From>> implements IListOutput<From, To> {
-    List<To> list;
-    Page<To> page;
+public abstract class BaseListOutput<Model, Bo extends BaseBo<Model>> implements IListOutput<Model, Bo> {
+    List<Bo> list;
+    Page<Bo> page;
     private Logger log = LoggerFactory.getLogger(BaseListOutput.class);
 
     @SuppressWarnings("unchecked")
     public BaseListOutput(Object from) {
         try {
             if (from instanceof List) {
-                this.list = fromModel((List<From>)from);
+                this.list = fromModel((List<Model>)from);
             }
 
             if (from instanceof Page)
-                fromModel((Page<From>) from);
+                fromModel((Page<Model>) from);
 
         } catch (Exception ex) {
             this.log.error(ExceptionUtils.getStackTrace(ex));
         }
     }
 
-    public BaseListOutput(Object from, To to) {
+    public BaseListOutput(Object from, Bo to) {
         try {
             if (from instanceof List) {
-                this.list = fromModel((List<From>) from, to);
+                this.list = fromModel((List<Model>) from, to);
             }
 
             if (from instanceof Page)
-                fromModel((Page<From>) from, to);
+                fromModel((Page<Model>) from, to);
         } catch (Exception ex) {
             this.log.error(ExceptionUtils.getStackTrace(ex));
         }
     }
 
-    public void fromModel(Page<From> from) throws Exception {
+    public void fromModel(Page<Model> from) throws Exception {
         this.list = fromModel(from.getContent());
-        this.page = new PageImpl<To>(this.list, new PageRequest(from.getNumber(), from.getSize()), from.getTotalElements());
+        this.page = new PageImpl<Bo>(this.list, new PageRequest(from.getNumber(), from.getSize()), from.getTotalElements());
     }
 
-    public void fromModel(Page<From> from, To to) throws Exception {
+    public void fromModel(Page<Model> from, Bo to) throws Exception {
         this.list = fromModel(from.getContent(), to);
-        this.page = new PageImpl<To>(this.list, new PageRequest(from.getNumber(), from.getSize()), from.getTotalElements());
+        this.page = new PageImpl<Bo>(this.list, new PageRequest(from.getNumber(), from.getSize()), from.getTotalElements());
     }
 
-    public Page<To> getPage() {
+    public Page<Bo> getPage() {
         return this.page;
     }
 
-    public List<To> getList() {
+    public List<Bo> getList() {
         return this.list;
     }
 }
