3593c84d283a16d88b2e2fc77763d20c97b4e288
wangwei <wangwei1008.ok@163.com>
2020-02-17 22:54:03 +0800

    auth common

---
+++ spring-boot-starter-blackey-mybatis/src/main/java/com/blackey/mybatis/model/BaseStringModel.java
@@ -0,0 +1,40 @@
+package com.blackey.mybatis.model;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableLogic;
+import com.baomidou.mybatisplus.extension.activerecord.Model;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+/**
+ * Created by Kaven
+ * Date: 2018/4/8
+ * Desc:
+ */
+@Setter
+@Getter
+public abstract class BaseStringModel<T extends Model> extends Model<T> {
+
+    @TableId
+    private String id;
+
+    @TableLogic
+    @TableField(fill = FieldFill.INSERT)
+    private Integer isDeleted;
+
+    @TableField(fill = FieldFill.INSERT)
+    private Date createdDate;
+
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Date updatedDate;
+
+    @TableField(fill = FieldFill.INSERT)
+    private String createdBy;
+
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private String updatedBy;
+}
--- spring-boot-starter-blackey-swagger/src/main/java/com/blackey/swagger/config/SwaggerAutoConfiguration.java
+++ spring-boot-starter-blackey-swagger/src/main/java/com/blackey/swagger/config/SwaggerAutoConfiguration.java
@@ -1,15 +1,19 @@
 package com.blackey.swagger.config;
 
+import com.google.common.base.Function;
+import com.google.common.base.Predicate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.RequestHandler;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Optional;
 
 /**
  * swagger2 配置
@@ -20,6 +24,10 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerAutoConfiguration {
+
+
+    // 定义分隔符
+    private static final String SPLITOR = ",";
 
     @Value("${blackey.swagger2.basePackage:com.blackey.invest}")
     String basePackage;
@@ -32,7 +40,7 @@ public class SwaggerAutoConfiguration {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.basePackage(basePackage))
+                .apis(basePackage(basePackage))
                 .paths(PathSelectors.any()).build();
     }
 
@@ -42,5 +50,29 @@ public class SwaggerAutoConfiguration {
                 .termsOfServiceUrl("https://github.com/")
                 .build();
     }
+
+
+    private static Predicate<RequestHandler> basePackage(final String basePackage) {
+        return input -> declaringClass(input).map(handlerPackage(basePackage)).orElse(true);
+    }
+
+    private static Function<Class<?>, Boolean> handlerPackage(final String basePackage)     {
+        return input -> {
+            // 循环判断匹配
+            for (String strPackage : basePackage.split(SPLITOR)) {
+                boolean isMatch = input.getPackage().getName().startsWith(strPackage);
+                if (isMatch) {
+                    return true;
+                }
+            }
+            return false;
+        };
+    }
+
+    private static Optional<? extends Class<?>> declaringClass(RequestHandler input) {
+        return Optional.ofNullable(input.declaringClass());
+    }
+
+
 
 }
